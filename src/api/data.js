d226a2976ab044aa82a9ce6e8062a4cac556dc34
maciejniedzielski <niedzielamac@gmail.com>
2019-03-02 18:23:34 +0100

    feat(general): Add new endpoints

--- src/api/data.js
+++ src/api/data.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import axios from 'axios';
+import dayjs from 'dayjs'
 
 export const dataEndpoints = Router();
 
 dataEndpoints.get('/standings', (req, res) => {
 	axios.get('http://data.nba.net/prod/v1/current/standings_conference.json')
-	.then(response => {
-		res.send(response.data);
+	.then(({ data }) => {
+		res.send(data['league'].standard.conference);
 	})
 	.catch(error => {
 		const customError = {
@@ -21,8 +22,8 @@ dataEndpoints.get('/standings', (req, res) => {
 
 dataEndpoints.get('/teams', (req, res) => {
 	axios.get('http://data.nba.net/prod/v1/2018/teams.json')
-		.then(response => {
-			res.send(response);
+		.then(({ data }) => {
+			res.send(data.league.standard.filter(team => team.isNBAFranchise));
 		})
 		.catch(error => {
 			const customError = {
@@ -35,10 +36,16 @@ dataEndpoints.get('/teams', (req, res) => {
 		});
 });
 
-dataEndpoints.get('/teams/:id', (req,res) => {
-	axios.get('http://data.nba.net/prod/v1/2016/teams/1610612745/roster.json')
-		.then(response => {
-			res.send(response);
+dataEndpoints.get('/teams/:id', (req,res) => {	
+	const teamId = req.params.id;
+		axios.all([
+			axios.get(`http://data.nba.net/prod/v1/2018/teams.json`),
+			axios.get('http://data.nba.net/prod/2018/teams_config.json')
+		]).then(([teamData, teamConfigData]) => {
+			const filteredTeamData = teamData.data.league.standard.find(team => team.teamId == teamId);
+			const filteredTeamConfigData = 	teamConfigData.data.teams.config.find(team => team.teamId == teamId)
+			const resultData = Object.assign(filteredTeamData, filteredTeamConfigData);
+			res.send(resultData);
 		})
 		.catch(error => {
 			const customError = {
@@ -51,4 +58,36 @@ dataEndpoints.get('/teams/:id', (req,res) => {
 		});
 });
 
+dataEndpoints.get('/scoreboard', (req, res) => {
+	axios.get(`http://data.nba.net/prod/v2/${ dayjs().format('YYYYMMDD') }/scoreboard.json`)
+		.then(({ data }) => {
+			res.send(data.games);
+		})
+		.catch(error => {
+			const customError = {
+				status: false,
+				data: {
+					msg: 'Problem with fetching scoreboard'
+				}
+			}
+			res.send(customError);
+		});
+});
 
+dataEndpoints.get('/players', (req, res) => {
+	axios.get(`https://www.nba.com/players/active_players.json`)
+		.then(({ data }) => {
+			res.send(data);
+		})
+		.catch(error => {
+			const customError = {
+				status: false,
+				data: {
+					msg: 'Problem with fetching active players'
+				}
+			}
+			res.send(customError);
+		});
+});
+
+// ^ .nugget player of the game w każdym obiekcie tego repsonsu, wiec łatwo znalezc jakiegos player of the day moze
--- src/api/stats.js
+++ src/api/stats.js
@@ -5,26 +5,32 @@ export const statsEndpoints = Router();
 
 statsEndpoints.get('/players', (req, res) => {
   axios.get('http://stats.nba.com/stats/commonallplayers?IsOnlyCurrentSeason=1&LeagueID=00&Season=2016-17')
-    .then(function (response) {
-      console.log(response.data);
-      res.send(response.data)
+    .then(({ data }) => {
+      res.send(data)
     })
     .catch(function (error) {
-      console.log(error);
+      const customError = {
+				status: false,
+				data: {
+					msg: 'Problem with fetching players stats'
+				}
+			}
+			res.send(customError);
     });
 });
 
 statsEndpoints.get('/player', (req, res) => {  
   axios.get('http://stats.nba.com/stats/commonplayerinfo/?playerId=201935&leagueId=00')
-    .then(function (response) {
-      console.log(response.data);
-      res.send(response.data)
+    .then(({ data }) => {
+      res.send(data)
     })
     .catch(function (error) {
-      console.log(error);
+      const customError = {
+				status: false,
+				data: {
+					msg: 'Problem with fetching player stats'
+				}
+			}
+			res.send(customError);
     });
 });
-
-statsEndpoints.get('/dupa', (req, res) => {
-  res.jsonp({'dupa': 1})
-})
